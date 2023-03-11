3826a69858a015dc754ffceffff00a9682001b75
Vazhid Gadzhiev <vazhid2000@mail.ru>
2023-03-11 11:26:21 +0300

    hw3 heap_class add

---
+++ module_1/hw3/main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+
+template <class T>
+class heap {
+private:
+    int size = 0;
+    int buffer_size = 3;
+    T* arr = new T[3];
+
+    void sift_down(int ind, bool (*is_less)(const T&, const T&)) {
+        int largest = 0;
+        int left = 0;
+        int right = 0;
+
+        while (1) {
+            largest = ind;
+            left = 2 * ind + 1;
+            right = 2 * ind + 2;
+
+            if (left <= size - 1 && is_less(arr[left], arr[ind]))
+                largest = left;
+            
+            if (right <= size - 1 && is_less(arr[right], arr[largest]))
+                largest = right;
+            
+            if (largest != ind) {
+                std::swap(arr[ind], arr[largest]);
+                ind = largest;
+            } else {
+                return;
+            }
+        }
+    }
+    
+    void grow_buffer() {
+        buffer_size *= 3;
+        T *arr_copy = new T[buffer_size];
+        std::copy(arr, arr + size, arr_copy);
+        delete[] arr;
+        arr = arr_copy;
+        arr_copy = nullptr;
+    }
+    
+
+    void sift_up(int ind, bool (*is_less)(const T&, const T&)) {
+        int parent;
+        while (ind > 0) {
+            parent = (ind - 1)/2;
+            if (is_less(arr[ind], arr[parent])) {
+                std::swap(arr[ind], arr[parent]);
+            }
+            ind = parent;
+        }
+    };
+    
+public:
+    heap() = default;
+    heap(const heap&) = delete;
+    heap& operator=(const heap& h) = delete;
+    ~heap() {
+        delete[] arr;
+    }
+
+    int get_size () {
+        return size;
+    }
+    
+    void add_element(const T& elem) {
+        if (size >= buffer_size) {
+            grow_buffer();
+        }
+        arr[size] = elem;
+        sift_up(size, is_less);
+        ++size;
+    }
+    
+    T extract_min() {
+        T min = arr[0];
+        arr[0] = arr[size - 1];
+        sift_down(0, is_less);
+        --size;
+        return min;
+    }
+};
+
+int main() {
+    
+}
