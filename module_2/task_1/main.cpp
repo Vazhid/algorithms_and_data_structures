74b379ac063f19ea22db35d8d5e288ef9fa31d53
vazhid <vazhid64@gmail.com>
2023-05-08 23:50:20 +0300

    mod2

--- module_2/task_1/main.cpp
+++ module_2/task_1/main.cpp
@@ -1,170 +1,148 @@
-// Copyright 2022 Vazhid
 #include<iostream>
+#include <fstream>
 #include<vector>
 #include<string>
-#include<cassert>
+
+#define EMP "EMP"
+#define KEY "KEY"
+#define DEL "DEL"
 
 struct StringHasher {
-    unsigned int operator()(const std::string& str) const {
-        unsigned int hash = 0;
-        for (unsigned int i = 0; i < str.size(); ++i) {
-            hash = hash * 17 + str[i];
+    size_t operator()(const std::string& str) const {
+        size_t hash = 0;
+        for(const char& ch : str) {
+            hash = (hash * 17 + ch);
         }
         return hash;
     }
 };
 
-template<class T, class H>
+template<typename Hasher>
 class HashTable {
- public:
-    explicit HashTable(const H& hasher) :
-        hasher_(hasher),
-        table_(8, nullptr),
-        keys_count_(0)
-        {}
-
-    HashTable(const HashTable&) = delete;
-    HashTable(HashTable&&) = delete;
-
-    HashTable& operator=(const HashTable&) = delete;
-    HashTable& operator=(HashTable&&) = delete;
-
-    ~HashTable();
-
-    bool Has(const T& key) const;
-    bool Add(const T& key);
-    bool Delete(const T& key);
-
  private:
     struct HashTableNode {
-        T data;
-        unsigned int hash;
-        HashTableNode* next;
+     public:
+        std::string data_;
+        std::string state_;
 
-        HashTableNode() : hash(0), next(nullptr) {}
-        HashTableNode(
-            const T& data,
-            unsigned int hash,
-            HashTableNode* next_node) :
-            data(data), hash(hash), next(next_node) {}
+        explicit HashTableNode(const std::string& data) : data_(data), state_(KEY) {}  
+        HashTableNode() : data_(""), state_(EMP) {}
     };
 
-    H hasher_;
-    std::vector<HashTableNode*> table_;
-    unsigned int keys_count_;
-    void growTable();
+    std::vector<HashTableNode> table_;
+    size_t keys_count_;
+    Hasher hasher_;
+
+    size_t probe(size_t hash, size_t i) {
+        return (hash + i) % table_.size();
+    }
+
+    void grow() {
+        std::vector<HashTableNode> old_table = std::move(table_);
+        table_.resize(old_table.capacity() * 2);
+        keys_count_ = 0;
+        for (const HashTableNode& node: old_table) {
+            if (node.state_ == KEY)
+                add(node.data_);
+        }
+    }
+    
+ public:
+    explicit HashTable() :
+        table_(8),
+        keys_count_(0) {}
+
+    bool add(const std::string& data) {
+        if (keys_count_ >= table_.size() * 0.75) {
+            grow();
+        }
+        int first_deleted = -1;
+        HashTableNode node;
+        size_t hash = hasher_(data);
+        size_t index;
+        for (size_t i = 0; i < table_.size(); i++) {
+            index = probe(hash, i);
+            node = table_[index];
+            if (node.data_ == data && node.state_ == KEY) {
+                return false;
+            }
+            if (node.state_ == DEL && first_deleted == -1) {
+                first_deleted = index;
+                continue;
+            }
+            if (node.state_ == EMP) {
+                HashTableNode set_node(data);
+                first_deleted == -1 ? table_[index] = set_node : table_[first_deleted] = set_node;
+                ++keys_count_;
+                return true;
+            }   
+        }
+        if (first_deleted != -1) {
+            HashTableNode set_node(data);
+            table_[first_deleted] = set_node;
+            ++keys_count_;
+            return true;
+        }
+        return false;
+    }
+
+    bool has(const std::string& data) {
+        size_t hash = hasher_(data);
+        size_t index;
+        for (size_t i = 0; i < table_.size(); i++) {
+            index = probe(hash, i);
+            HashTableNode& node = table_[index];
+            if (node.data_ == data && node.state_ == KEY) {
+                return true;
+            }
+            if (node.state_ == EMP) {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    bool remove(const std::string& data) {
+        size_t hash = hasher_(data);
+        size_t index;
+        for (size_t i = 0; i < table_.size(); i++) {
+            index = probe(hash, i);
+            HashTableNode node = table_[index];
+            if (node.data_ == data && node.state_ == KEY) {
+                table_[index].state_ = DEL;
+                --keys_count_;
+                return true;
+            }
+            if (node.state_ == EMP) {
+                return false;
+            }
+        }
+        return false;
+    }
 };
-
-template<class T, class H>
-HashTable<T, H>::~HashTable() {
-    for (unsigned int i = 0; i < table_.size(); ++i) {
-        HashTableNode* node = table_[i];
-        while (node != nullptr) {
-            HashTableNode* next_node = node->next;
-            delete node;
-            node = next_node;
-        }
-    }
-}
-
-template<class T, class H>
-bool HashTable<T, H>::Has(const T& data) const {
-    unsigned int hash = hasher_(data) % table_.size();
-    HashTableNode* node = table_[hash];
-    while (node != nullptr && node->data != data) {
-        node = node->next;
-    }
-    return node != nullptr;
-}
-
-template<class T, class H>
-bool  HashTable<T, H>::Add(const T& data) {
-    if (keys_count_ > 3 * table_.size()) {
-        growTable();
-    }
-
-    unsigned int abs_hash = hasher_(data);
-    unsigned int hash = abs_hash % table_.size();
-    HashTableNode* node = table_[hash];
-    while (node != nullptr && node->data != data)
-        node = node->next;
-
-    if (node != nullptr) {
-        return false;
-    }
-
-    table_[hash] = new HashTableNode(data, abs_hash, table_[hash]);
-    ++keys_count_;
-    return true;
-}
-
-template<class T, class H>
-bool HashTable<T, H>::Delete(const T& data) {
-    unsigned int hash = hasher_(data) % table_.size();
-    HashTableNode* node = table_[hash];
-    HashTableNode* prev_node = nullptr;
-
-    while (node != nullptr && node->data != data) {
-        prev_node = node;
-        node = node->next;
-    }
-
-    if (node == nullptr) {
-        return false;
-    }
-
-    if (prev_node == nullptr) {
-        table_[hash] = node->next;
-    } else {
-        prev_node->next = node->next;
-    }
-
-    delete node;
-    --keys_count_;
-    return true;
-}
-
-template<class T, class H>
-void HashTable<T, H>::growTable() {
-    std::vector<HashTableNode*> new_table(table_.size() * 2, nullptr);
-    for (unsigned int i = 0; i < table_.size(); ++i) {
-        HashTableNode* node = table_[i];
-        while (node != nullptr) {
-            HashTableNode* next_node = node->next;
-            unsigned int new_hash = node->hash % new_table.size();
-            node->next = new_table[new_hash];
-            new_table[new_hash] = node;
-            node = next_node;
-        }
-    }
-    table_ = std::move(new_table);
-}
 
 int main() {
     StringHasher hasher;
-    HashTable<std::string, StringHasher> my_table(hasher);
-    char operation_number = 0;
+    HashTable<StringHasher> my_table;
+    char operation_number;
     std::string my_data;
     while (std::cin >> operation_number >> my_data) {
         switch (operation_number) {
         case '+':
             std::cout
-                << (my_table.Add(my_data) ? "OK" : "FAIL")
+                << (my_table.add(my_data) ? "OK" : "FAIL")
                 << std::endl;
             break;
         case '-':
             std::cout
-                << (my_table.Delete(my_data) ? "OK" : "FAIL")
+                << (my_table.remove(my_data) ? "OK" : "FAIL")
                 << std::endl;
             break;
         case '?':
             std::cout
-            << (my_table.Has(my_data) ? "OK" : "FAIL")
+            << (my_table.has(my_data) ? "OK" : "FAIL")
             << std::endl;
             break;
-        default:
-            assert(false);
         }
     }
-    return 0;
 }
